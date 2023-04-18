e26cc0fc80d904cff5d3b9efe976b41391d09318
Anuar Rozman <anuarrozman2303@gmail.com>
2023-04-18 16:16:53 +0800

    Updated version of python program
    - filter & comparison works well

--- test.py
+++
@@ -1,9 +0,0 @@
-# Open the file in binary mode for reading
-with open('C:\\Python\\IR Compare\\Daikin\\Commands\\default.txt', 'rb') as file:
-    contents = file.read()
-    # Convert to a string
-    contents_str = contents.decode('utf-8')
-    # Filter out all characters that are not '0' or '1'
-    filtered_str = ''.join(c for c in contents_str if c in ('0', '1'))
-    # Print the filtered string
-    print(filtered_str)
