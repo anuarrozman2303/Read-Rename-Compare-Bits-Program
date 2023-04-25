531776d5f841e38af4f0dae043b95603a325615d
Anuar Rozman <anuarrozman2303@gmail.com>
2023-04-25 17:20:44 +0800

    testing test3.py adding chsum
    - 25April_FileCompare.py is working as intended.
    - test3.py in progress of adding chsum label.

--- test3.py
+++ test3.py
@@ -1,33 +1,32 @@
 import os
 import configparser
 
-# Create a new directory called "OutputTest3" if it doesn't exist
-if not os.path.exists('OutputTest3'):
-    os.makedirs('OutputTest3')
-
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+# Create the output directory if it doesn't exist
+if not os.path.exists('Output25April'):
+    os.makedirs('Output25April')
+
 # Loop through each section in the config file
 for section in config.sections():
-    section_items = list(config.items(section))
     if section in ['on_off', 'temp', 'mode', 'fanspeed', 'vswing', 'hswing', 'special', 'comfort']:
-        # Create a new output file for this section in the "OutputTest3" directory
-        filename = os.path.join('OutputTest3', f"{section}.txt")
-        with open(filename, 'w') as output:
-            section_items = list(config.items(section))
-            output.write(f"--- {section} ---\n")
+        section_items = list(config.items(section))
+        output_path = f'Output25April/{section}.txt'
+        # Open the output file for writing
+        with open(output_path, 'w') as output:
+            output_lines = []
+            output_lines.append(f'--- {section} ---\n\n')
             first_file = section_items[0][1]
-            frame_break_count = [0, 0]  # counter variable for "Frame Break"
             for item in section_items[1:]:
                 current_file = item[1]
                 keys = [section_items[0][0], item[0]]
-                output.write(f"Comparing {keys[0]} with {keys[1]}\n")
+                output_lines.append(f'Comparing {keys[0]} with {keys[1]}:\n\n')
                 with open(first_file, 'r') as f1, open(current_file, 'r') as f2:
                     lines1 = f1.readlines()
                     lines2 = f2.readlines()
                     # Filter process for file1
-                    for i in range(len(lines1)):
+                    for i in range (len(lines1)):
                         lines1[i] = lines1[i].split(':', 1)[-1]
                         lines1[i] = ''.join([c for c in lines1[i] if not c.isalpha()])
                         lines1[i] = lines1[i].replace(':', '', 1)
@@ -39,19 +38,19 @@ for section in config.sections():
                     # Remove empty lines & move up the data.
                     lines1 = [line.strip() for line in lines1 if line.strip()]
                     lines2 = [line.strip() for line in lines2 if line.strip()]
-                    output_lines = []
                     # Print the comparison results horizontally
                     for i in range(min(len(lines1), len(lines2))):
-                        output_lines.append(f'{lines1[i].strip():<40}{lines2[i].strip()}')
-                        if i % 64 == 63:
-                            if frame_break_count[i // 64] < 2:  # print "Frame Break" twice
-                                output_lines.append('Frame Break')
-                                frame_break_count[i // 64] += 1
-                            else:
-                                frame_break_count[i // 64] = 0  # reset counter after second "Frame Break"
-                    output.write('\n'.join(output_lines) + '\n\n')
-
-                    # File errors warning.
-                    # Can remove if unnecessary.
+                        if i == 56 or i == 120 or i == 271:
+                            output_lines.append('ChSum'.ljust(40) + '|' + f'{i+1:02d}'.rjust(3) + '|' + lines1[i].strip().ljust(28) + '|' + lines2[i].strip())
+                        else:
+                            output_lines.append(lines1[i].strip().ljust(40) + '|' + f'{i+1:02d}'.rjust(3) + '|' + lines2[i].strip())
+                    # Add warning messages for file errors
                     if len(lines1) != len(lines2):
-                            output.write('Warning: Files have different number of lines\n\n')
+                        output_lines.append('\nWarning: Files have different number of lines')
+                    if len(lines1) == 0:
+                        output_lines.append('\nError: File 1 is empty')
+                    if len(lines2) == 0:
+                        output_lines.append('\nError: File 2 is empty')
+                    output_lines.append('\n\n')
+            # Write all output lines to the file
+            output.writelines(output_lines)
